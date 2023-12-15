f13f9e28bfa800a186240a9c1f8c6f26457b2e23
Dmytro Bihniak <bignyak@bigmir.net>
2023-12-15 02:40:19 +0100

    Day 1.1

--- 2015/01.lisp
+++ 2015/01.lisp
@@ -1,3 +1,27 @@
 (in-package :aoc-2015-01)
 
-(aoc:define-day nil nil)
+(aoc:define-day 232 nil)
+
+;; Input
+
+(defvar *input* (aoc:input))
+
+;; Part 1
+
+(defun count-floor (paren-string)
+  (loop for paren across paren-string
+        sum (if (char= paren #\() 1 -1)))
+
+(defun get-answer-1 (&optional (instructions *input*))
+  (count-floor instructions))
+
+(aoc:given 1
+  (= 0 (get-answer-1 "(())"))
+  (= 0 (get-answer-1 "()()"))
+  (= 3 (get-answer-1 "((("))
+  (= 3 (get-answer-1 "(()(()("))
+  (= 3 (get-answer-1 "))((((("))
+  (= -1 (get-answer-1 "())"))
+  (= -1 (get-answer-1 "))("))
+  (= -3 (get-answer-1 ")))"))
+  (= -3 (get-answer-1 ")())())")))
