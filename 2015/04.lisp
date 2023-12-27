947de8df43c236bbd1ff12c3a153824132b66d2e
Dmytro Bihniak <bignyak@bigmir.net>
2023-12-27 02:31:25 +0100

    Improve speed of day4 by not converting md5 sum to string

--- 2015/04.lisp
+++ 2015/04.lisp
@@ -1,5 +1,6 @@
 (in-package :aoc-2015-04)
 
+;; Part 2 takes about 5 seconds to run on my machine.
 (aoc:define-day 282749 9962624)
 
 ;; Input
@@ -8,19 +9,18 @@
 
 ;; Part 1
 
-(defun md5 (string)
-  (format nil "~(~{~2,'0X~}~)"
-          (map 'list #'identity (md5:md5sum-string string))))
-
 (defun mine-hash (algorithm secret-key)
   (flet ((hash-probe (n)
            (format nil "~A~A" secret-key n)))
     (do ((i 1 (1+ i))
-         (hash (md5 (hash-probe 1)) (md5 (hash-probe i))))
+         (hash (md5:md5sum-string (hash-probe 1)) (md5:md5sum-string (hash-probe i))))
         ((funcall algorithm hash) (1- i)))))
 
 (defun get-answer-1 (&optional (secret-key *secret-key*))
-  (let ((difficalty-function (lambda (hash) (string= (subseq hash 0 5) "00000"))))
+  (let ((difficalty-function (lambda (hash)
+                               (and (= (aref hash 0) 0)
+                                    (= (aref hash 1) 0)
+                                    (= (ash (aref hash 2) -4) 0)))))
     (mine-hash difficalty-function secret-key)))
 
 (aoc:given 1
@@ -30,5 +30,8 @@
 ;; Part 2
 
 (defun get-answer-2 (&optional (secret-key *secret-key*))
-  (let ((difficalty-function (lambda (hash) (string= (subseq hash 0 6) "000000"))))
+  (let ((difficalty-function (lambda (hash)
+                               (and (= (aref hash 0) 0)
+                                    (= (aref hash 1) 0)
+                                    (= (aref hash 2) 0)))))
     (mine-hash difficalty-function secret-key)))
