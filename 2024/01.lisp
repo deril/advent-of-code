f90ad9766eafde21fc9e79927e851ea1dbe71f04
Dmytro Bihniak <bignyak@bigmir.net>
2024-12-11 11:38:03 +0100

    Use gmap instead of custom map reduce

--- 2024/01.lisp
+++ 2024/01.lisp
@@ -26,16 +26,14 @@
 
 ;;; Part 1
 
-(defun sum-over-two-seqs (seq1 seq2 func)
-  (iter
-    (for x in-sequence seq1)
-    (for y in-sequence seq2)
-    (sum (funcall func x y))))
-
 (defun get-answer-1 (&optional (lists *lists*))
   (let ((seq1 (sort (copy-list (first lists)) #'<))
         (seq2 (sort (copy-list (second lists)) #'<)))
-    (sum-over-two-seqs seq1 seq2 #'(lambda (x y) (abs (- x y))))))
+    (gmap:gmap
+     (:result :sum)
+     #'(lambda (x y) (abs (- x y)))
+     (:arg list seq1)
+     (:arg list seq2))))
 
 (aoc:given 1
   (= 11 (get-answer-1 *example*)))
@@ -48,8 +46,10 @@
 (defun get-answer-2 (&optional (lists *lists*))
   (let ((seq1 (first lists))
         (seq2 (list-to-bag (second lists))))
-    (iter (for x in-sequence seq1)
-      (sum (* x (fset:multiplicity seq2 x))))))
+    (gmap:gmap
+     (:result :sum)
+     #'(lambda (x) (* x (fset:multiplicity seq2 x)))
+     (:arg list seq1))))
 
 (aoc:given 2
   (= 31 (get-answer-2 *example*)))
