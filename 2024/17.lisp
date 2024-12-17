c4c4544d422508cfb458c95f2de4da09a87eec8d
Dmytro Bihniak <bignyak@bigmir.net>
2024-12-17 21:28:45 +0100

    Day 17 part 2

--- 2024/17.lisp
+++ 2024/17.lisp
@@ -1,6 +1,6 @@
 (in-package :aoc-2024-17)
 
-(aoc:define-day nil nil)
+(aoc:define-day "4,1,7,6,4,1,0,2,7" 164279024971453) ;; TODO Part 2 solution is very tailored to the input
 
 ;;; Parsing
 
@@ -43,7 +43,8 @@
                     ((0 1 2 3) operand)
                     (4 :A)
                     (5 :B)
-                    (6 :C))))
+                    (6 :C)
+                    (otherwise (error "Invalid operand")))))
     (if (keywordp register)
         (getf registers register)
         operand)))
@@ -100,21 +101,12 @@
     (nth pointer program)))
 
 (defun compute (op1 op2 registers pointer)
-  (let ((command (case op1
-                   (0 'adv)
-                   (1 'bxl)
-                   (2 'bst)
-                   (3 'jnz)
-                   (4 'bxc)
-                   (5 'out)
-                   (6 'bdv)
-                   (7 'cdv))))
-    (reveal `(,command ,op2 ',registers ,pointer))))
-
-(defun reveal (command)
   (let* ((output-stream (make-string-output-stream))
-         (*standard-output* output-stream))
-    (list (eval command) (get-output-stream-string output-stream))))
+         (*standard-output* output-stream)
+         (command (vector 'adv 'bxl 'bst 'jnz 'bxc 'out 'bdv 'cdv)))
+    (list
+     (funcall (aref command op1) op2 registers pointer)
+     (get-output-stream-string output-stream))))
 
 (defun recur (program registers)
   (loop for (pointer output) = '(0 "") then (compute op1 op2 registers pointer)
@@ -125,7 +117,35 @@
         finally (return (format nil "~{~A~^,~}" (remove "" outputs :test #'string=)))))
 
 (defun get-answer-1 (&optional (input *input*))
-  (recur (second input) (copy-seq (first input) )))
+  (destructuring-bind (registers program) input
+    (recur program (copy-seq registers))))
 
 (aoc:given 1
   (string= "4,6,3,5,6,3,5,2,1,0" (get-answer-1 *example*)))
+
+;;; Part 2
+
+(defun run-with-register-a (registers program a)
+  (let ((registers (copy-seq registers)))
+    (setf (getf registers :A) a)
+    (recur program registers)))
+
+(defun find-self-copy (registers program)
+  (let ((candidates (fset:seq 0))
+        (min-candidate (expt 2 (* 3 (1- (length program))))))
+    (loop while (and (fset:nonempty? candidates)
+                     (< (fset:last candidates) min-candidate))
+          for seed = (fset:pop-first candidates)
+          do (loop for a from 0 below (expt 2 6)
+                   do (let* ((a (+ (ash seed 6) a))
+                             (result (aoc:extract-ints (run-with-register-a registers program a))))
+                        (when (< a 8)
+                          (setq result (cons 0 '(result))))
+                        (when (equal result (subseq program (- (length program) (length result))))
+                          (setq candidates (fset:with-last candidates a)))
+                        (when (equal program result)
+                          (return-from find-self-copy (fset:last candidates))))))))
+
+(defun get-answer-2 (&optional (input *input*))
+  (destructuring-bind (registers program) input
+    (find-self-copy registers program)))
